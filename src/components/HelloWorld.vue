f7bdefa797bddfa0c767f3fe569499539dfde5d3
tranChiVi0123 <idiots1230@gmail.com>
2022-08-04 04:01:37 +0900

    add title

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>Money Forward</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
